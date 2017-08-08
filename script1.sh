34061bf82f078b52bf241f3e568fb5d913a1fc91
semhc12 <semhc12@condor-headnode.cloud.univalle.edu.co>
2017-08-08 17:18:24 -0500

    new file:   script1.sh

---
+++ script1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ls -l /home/invitado/Scratch/exercise-01-02
+
+mv *.sh /home/invitado/Scratch/exercise-01-02/tmp
+
+chmod +x *.sh
+
+cd /home/invitado/Scratch/exercise-01-02/tmp 
+
+ls -l
+
+done
+
